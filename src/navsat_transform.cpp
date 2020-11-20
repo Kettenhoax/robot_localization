6f7b294ada0496ee8377cd560b9ebb8e669d4acb
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2020-11-20 11:41:01 +0100

    Set GPS odom child frame

--- src/navsat_transform.cpp
+++ src/navsat_transform.cpp
@@ -405,6 +405,7 @@ nav_msgs::msg::Odometry NavSatTransform::utmToMap(
   // position at that timestamp
   gps_odom.header.frame_id = world_frame_id_;
   gps_odom.header.stamp = gps_update_time_;
+  gps_odom.child_frame_id = base_link_frame_id_;
 
   // Now fill out the message. Set the orientation to the identity.
   tf2::toMsg(transformed_utm_gps, gps_odom.pose.pose);
